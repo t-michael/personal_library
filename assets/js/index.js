770a44d2b58b2f88b5746ec057fe5961174960f0
t-michael <michaelt@michaelt.dev>
2022-08-25 21:13:17 -0500

    began adding add book button functionality

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,8 +6,14 @@ function Book(author, pageCount, bookTitle, ) {
 
 }
 
+function addNewBook(ele) {
+    const displayBookInput = document.getElementById("#add_new_book");
+    if (displayBookInput.style.display == "block") {
+        displayBookInput.style.display = "none";
 
-
-function addBookBtn() {
-    console.log("Hello");
+        document.getElementById(ele.id).value = "Show Input";
+    } else {
+        displayBookInput.style.display = "block";
+        document.getElementById(ele.id).value = "Hide Input";
+    }
 }
