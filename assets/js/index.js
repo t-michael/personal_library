b42306b8ab586ab8dccb31f70604424c38f35da4
t-michael <michaelt@michaelt.dev>
2022-09-07 21:40:45 -0500

    created Book constructor

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,16 +4,31 @@ const addBookBtn = document.getElementById("add_new_book");
 // Create library array
 let userLibrary = [];
 
-// Book Constructor
+// Book constructor
 function Book(author, pageCount, bookTitle) {
+    this.author = author
+    this.pageCount = pageCount
+    this.bookTitle = bookTitle
 }
 
-// Add a Book to Library
+// Add a book to library
 function addBookToLibrary() {
-   
+   const userInput = document.getElementById("submit_btn").value;
+
+   userLibrary.push(submit_btn);
+
+   const cur_library = "";
+
+   for(i = 0; i < userLibrary.length; i++) {
+    cur_library = cur_library + userLibrary[i];
+   }
+
+   document.getElementById("book_card").textContent = cur_library;
+   console.log(cur_library);
+   console.log(userLibrary);
 }
 
-// Add a Book Form
+// Add a book form
 function addNewBook() {
         if (bookForm.style.display === "none") {
             bookForm.style.display = "block";
