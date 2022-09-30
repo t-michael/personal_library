3ccf37ea559b3aef339a8332efcc875c4f7306de
t-michael <michaelt@michaelt.dev>
2022-09-30 11:10:20 -0500

    fixed book form displaying on-load

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const bookForm = document.getElementById("add_book_form_container");
 const addBookBtn = document.getElementById("add_new_book");
 const submitBook = document.getElementById("submit_btn");
 submitBook.addEventListener("click", addBookToLibrary());
+bookForm.addEventListener("click", addNewBook, false);
+
 
 // Add a book to the library
 function addBookToLibrary () {
@@ -19,13 +21,14 @@ function addBookToLibrary () {
 }
 
 // Add a book form
-// function addNewBook() {
-//     if (bookForm.style.display === "none") {
-//         bookForm.style.display = "block";
-//     } else {
-//         bookForm.style.display = "none";
-//     }
-// };
+function addNewBook() {
+    if (bookForm.style.display === "none") {
+        bookForm.style.display = "block";
+    } else {
+        bookForm.style.display = "none";
+    }
+};
+
 
 // Book constructor
 function Book(author, pageCount, bookTitle) {
